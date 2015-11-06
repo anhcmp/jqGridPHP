25598bb9f87ed22628bb4bc1456f95f013bed3b7
Phuoc Chau <chau.phuoc@hotmail.com>
2015-11-06 15:38:33 +0700

    #comunity version of backend jqgrid

--- examples/config.php
+++ examples/config.php
@@ -1,12 +1,15 @@
 <?php
-$lang = 'ru';
+$lang = 'en';
+
+$Server = "103.249.162.130";
+$DbUserName = "suga_user";
+$DbPassword = 'Pw$suga@123';
+$DbName = "cas_db";
 
 $_CONFIG = array(
     'root_path' => dirname(__FILE__) . '/../php/',
-
     'encoding' => 'utf-8',
-
-    'pdo_dsn' => 'mysql:dbname=jqgrid_test;host=127.0.0.1',
-    'pdo_user' => 'root',
-    'pdo_pass' => '',
+    'pdo_dsn' => "sqlsrv:Server=$Server;Database=$DbName",
+    'pdo_user' => "$DbUserName",
+    'pdo_pass' => "$DbPassword",
 );
